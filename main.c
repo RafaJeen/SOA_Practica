fd57e1645fd908853b855e53e7eb371a514995e9
rafa <rafael.morera@students.salle.url.edu>
2021-04-12 19:44:33 +0200

    Prueba1

--- main.c
+++ main.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 
 int main (int argc, char *argv[]){
 
     if(argc != 3){
         printf("El nombre de parametres es incorrecte.\n");
+        return 0;
     }
 
-    if(strcmp(argv[1], "/info") != 0){
-        printf("La comanda %s es incorrecte\n", argv[1];)
+    if(strcmp(argv[1], "/intro") != 0){
+        printf("La comanda %s es incorrecte\n", argv[1]);
+        return 0;
     }
 
-    
+    int fd = open(argv[2], O_RDONLY);
+
+    if(fd < 0){
+        printf("Aquest error noexiteix.\n");
+        return 0;
+    } else {
+        printf("LO HE ABIERTO!!!!\n");
+    }
 
 
+    lseek(fd, 1024, SEEK_SET);
 
+    int num;
 
+    read(fd, &num, 4);
+
+    printf("%d\n", num);
 
 }
