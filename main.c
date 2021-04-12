f0679b1d934a35d7ec054ae8f2f8af4835bbd39d
rafa <rafael.morera@students.salle.url.edu>
2021-04-12 13:07:46 +0200

    init commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main (int argc, char *argv[]){
+
+    if(argc != 3){
+        printf("El nombre de parametres es incorrecte.\n");
+    }
+
+    if(strcmp(argv[1], "/info") != 0){
+        printf("La comanda %s es incorrecte\n", argv[1];)
+    }
+
+    
+
+
+
+
+
+}
